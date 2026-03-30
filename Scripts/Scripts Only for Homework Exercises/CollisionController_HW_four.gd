1cf4e8e6aef25da6d5bd255e4eab5275f8d64180
claireylee <157722103+claireylee@users.noreply.github.com>
2026-03-30 00:04:08 -0400

    Update

--- Scripts/Scripts Only for Homework Exercises/CollisionController_HW_four.gd
+++ Scripts/Scripts Only for Homework Exercises/CollisionController_HW_four.gd
@@ -10,3 +10,19 @@ func _disable_collision():
 
 func _enable_collision():
 	$CollisionShape2D.set_deferred("disabled", false)
+
+
+func _on_area_2d_body_entered(body: Node2D):
+	_enable_collision() # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D):
+	_disable_collision() # Replace with function body.
+
+
+func _on_open_door_button_pressed() -> void:
+	_enable_collision() # Replace with function body.
+
+
+func _on_close_door_button_pressed() -> void:
+	_disable_collision() # Replace with function body.
--- Scripts/Scripts Only for Homework Exercises/ShowHide_HW_four.gd
+++ Scripts/Scripts Only for Homework Exercises/ShowHide_HW_four.gd
@@ -9,10 +9,10 @@ func _on_door_area_body_exited(body):
 	if body.name == "CharacterBody2D":
 		door._show()
 func _on_open_door_button_pressed():
-	door._hide()
+	_hide()
 
 func _on_close_door_button_pressed():
-	door._show()
+	_show()
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,3 +23,11 @@ func _show():
 
 func _hide():
 	self.visible = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	_show() # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	_hide()# Replace with function body.
